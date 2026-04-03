2ce5ce5a61f7fc45034dd7ac29ce8a55d2e31539
cipgdfsasmpt-tech <cipgdfsa.smpt@gmail.com>
2026-04-03 12:51:05 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 
 const CIPGD_CONFIG = {
   URL_4RODAS: 'https://script.google.com/macros/s/AKfycbyiHrPSYc6HqysK2PCwWlzcvW7CsjnALjNdlgvEFYa6zsi1Jam8bFywENYJiLAA0dEMxw/exec',
-  URL_2RODAS: 'https://script.google.com/macros/s/AKfycbxcdbxeo6OhC9VM2zaYhiZCYWTDsOBqtjdDSCEKmcz0tZQm47J_lC2N0-xCgXPIkUbC/exec'
+  URL_2RODAS: 'https://script.google.com/macros/s/AKfycbyuDYSzGmTfmBctEclx4BVIYuGkFNxjY7nBEjNF7fyOjLRPZHcL2XxP0e6C0wZ0L1DNjA/exec'
 };

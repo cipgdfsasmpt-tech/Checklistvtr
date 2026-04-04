4ce20ea1a70914a53dc5c54d3577d9c8e1479486
cipgdfsasmpt-tech <cipgdfsa.smpt@gmail.com>
2026-04-04 01:38:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ const CIPGD_CONFIG = {
   VERSAO:    'v2.1',
   CACHE:     'cipgd-v2.1',
   URL_4RODAS: 'https://script.google.com/macros/s/AKfycbxGh7SfJU2rQusk6vyDWI6GwQObfLoaM3xWlzCZHksDKs28xiV8SzI9JCtZXfzylrg9Lg/exec',
-  URL_2RODAS: 'https://script.google.com/macros/s/AKfycbxXnITA3Mk2VmiX9Vfc2dcp977q934wTmQC4g5iFiF8Hi3HwzgyCCsbO2Kxd0BMuufOGw/exec'
+  URL_2RODAS: 'https://script.google.com/macros/s/AKfycbzDCJeSsZn_1AOv5-bXwUanM65buLK-BLfjU76_h8-BSilhlmfaSMFIyUclEsz9NkXGug/exec'
 };

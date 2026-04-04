ba994c54fe936caebbc8ec4df120b9c55e61c3ea
cipgdfsasmpt-tech <cipgdfsa.smpt@gmail.com>
2026-04-03 22:26:01 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 
 const CIPGD_CONFIG = {
   URL_4RODAS: 'https://script.google.com/macros/s/AKfycbxGh7SfJU2rQusk6vyDWI6GwQObfLoaM3xWlzCZHksDKs28xiV8SzI9JCtZXfzylrg9Lg/exec',
-  URL_2RODAS: ''https://script.google.com/macros/s/AKfycbxXnITA3Mk2VmiX9Vfc2dcp977q934wTmQC4g5iFiF8Hi3HwzgyCCsbO2Kxd0BMuufOGw/exec
+  URL_2RODAS: 'https://script.google.com/macros/s/AKfycbwFxYF1aHL85Mtu4MnmBxUu5CxCZ4dw1c43v5v8n4x-awHZmt8DvO8rfPYK6ZsQ_36NZw/exec'
 };
